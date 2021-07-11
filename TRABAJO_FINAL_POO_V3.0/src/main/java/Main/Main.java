482f4de166fd4dc8bf5b14c2bdc0a9f1c984149d
Amy2000gar <87243995+Amy2000gar@users.noreply.github.com>
2021-07-10 22:34:50 -0500

    Add files via upload

--- TRABAJO_FINAL_POO_V3.0/src/main/java/Main/Main.java
+++ TRABAJO_FINAL_POO_V3.0/src/main/java/Main/Main.java
@@ -2,11 +2,11 @@ package Main;
 import Vista.*;
 
 public class Main 
-{
+{ 
     public static void main(String[] args) 
     {
+        //holaaaaaaaaaaaaaaaaa rick xD
         Principal principal = new Principal();
         principal.setVisible(true);
-        System.out.println("Hola....");
     }
 }
